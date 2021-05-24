139638c829d55da0ff9c70517f13dc006bdaaeeb
Edcarl <carlmoyamoya@gmail.com>
2021-05-24 18:56:31 +0800

    Check winner

--- Form1.cs
+++ Form1.cs
@@ -12,7 +12,7 @@ namespace Tic_tac_toe
 {
     public partial class Form1 : Form
     {
-        bool turn;
+        bool turn = true;
 
         public Form1()
         {
@@ -31,6 +31,7 @@ namespace Tic_tac_toe
                 btn1.Text = "O";
                 turn = true;
             }
+            btn1.Enabled = false;
         }
 
         private void btn2_Click(object sender, EventArgs e)
@@ -45,6 +46,7 @@ namespace Tic_tac_toe
                 btn2.Text = "O";
                 turn = true;
             }
+            btn2.Enabled = false;
         }
 
         private void btn3_Click(object sender, EventArgs e)
@@ -59,6 +61,8 @@ namespace Tic_tac_toe
                 btn3.Text = "O";
                 turn = true;
             }
+            btn3.Enabled = false;
+            Check_winner();
         }
 
         private void btn4_Click(object sender, EventArgs e)
@@ -73,6 +77,7 @@ namespace Tic_tac_toe
                 btn4.Text = "O";
                 turn = true;
             }
+            btn4.Enabled = false;
         }
 
         private void btn5_Click(object sender, EventArgs e)
@@ -87,6 +92,7 @@ namespace Tic_tac_toe
                 btn5.Text = "O";
                 turn = true;
             }
+            btn5.Enabled = false;
         }
 
         private void btn6_Click(object sender, EventArgs e)
@@ -101,6 +107,8 @@ namespace Tic_tac_toe
                 btn6.Text = "O";
                 turn = true;
             }
+            btn6.Enabled = false;
+            Check_winner();
         }
 
         private void btn7_Click(object sender, EventArgs e)
@@ -115,6 +123,7 @@ namespace Tic_tac_toe
                 btn7.Text = "O";
                 turn = true;
             }
+            btn7.Enabled = false;
         }
 
         private void btn8_Click(object sender, EventArgs e)
@@ -129,6 +138,7 @@ namespace Tic_tac_toe
                 btn8.Text = "O";
                 turn = true;
             }
+            btn8.Enabled = false;
         }
 
         private void btn9_Click(object sender, EventArgs e)
@@ -142,6 +152,38 @@ namespace Tic_tac_toe
             {
                 btn9.Text = "O";
                 turn = true;
+            }
+            btn9.Enabled = false;
+            Check_winner();
+        }
+        
+        private void Check_winner()
+        {
+            bool winner = false;
+
+            if((btn1.Text == btn2.Text) && (btn2.Text == btn3.Text) && btn1.Enabled == false)
+            {
+                winner = true;
+            }
+            if ((btn4.Text == btn5.Text) && (btn5.Text == btn6.Text) && btn4.Enabled == false)
+            {
+                winner = true;
+            }
+            if ((btn7.Text == btn8.Text) && (btn8.Text == btn9.Text) && btn7.Enabled == false)
+            {
+                winner = true;
+            }
+
+            if (winner)
+            {
+                if (turn)
+                {
+                    MessageBox.Show("Player 2 WINS");
+                }
+                else
+                {
+                    MessageBox.Show("Player 1 WINS");
+                }
             }
         }
     }
